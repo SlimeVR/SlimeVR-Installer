0848057ccdb92a6dfbe086c1ac35867646871555
Yury <rip95_95@mail.ru>
2021-09-23 19:31:04 +0300

    Update steamvr.ps1

--- windows/web/steamvr.ps1
+++ windows/web/steamvr.ps1
@@ -1,26 +1,31 @@
-[CmdletBinding(PositionalBinding=$true)]
+[CmdletBinding(PositionalBinding=$false)]
 param (
     [parameter(Position=0)][string]$SteamPath,
     [parameter(Position=1)][string]$DriverPath,
-    [parameter(Position=2)][string]$Action,
-    [parameter(Position=3)][string]$SystemType
+    [parameter(Position=2)][switch]$Uninstall = $false
 )
 
-function Call-VrPathReg([string]$Path) {
-    Start-Process -FilePath $Path -ArgumentList "$Action", "`"$DriverPath`"" -Wait
-}
-
-if ((Test-Path -Path "$SteamPath\steamapps\common\SteamVR\bin\$SystemType\vrpathreg.exe") -eq $true) {
-    Call-VrPathReg -Path "$SteamPath\steamapps\common\SteamVR\bin\$SystemType\vrpathreg.exe"
+$DriverFolder = Split-Path -Path $DriverPath -Leaf
+$SteamVrPath = "$SteamPath\steamapps\common\SteamVR"
+if ((Test-Path -Path "$SteamVrPath\bin") -eq $true) {
+    if ($Uninstall -eq $true) {
+        Remove-Item -Recurse -Path "$SteamVrPath\drivers\$DriverFolder"
+        return 0
+    }
+    Copy-Item -Recurse -Force -Path $DriverPath -Destination "$SteamVrPath\drivers"
     return 0
 }
 
 $res = Select-String -Path "$SteamPath\steamapps\libraryfolders.vdf" -Pattern '"path"\s+"(.+?)"' -AllMatches
 foreach ($Match in $res.Matches) {
     $LibraryPath = $Match.Groups[1] -replace "\\\\", "\"
-    $SteamVrBinPath = "$LibraryPath\steamapps\common\SteamVR\bin"
-    if ((Test-Path -Path $SteamVrBinPath) -eq $true) {
-        Call-VrPathReg -Path "$SteamVrBinPath\$SystemType\vrpathreg.exe"
+    $SteamVrPath = "$LibraryPath\steamapps\common\SteamVR"
+    if ((Test-Path -Path "$SteamVrPath\bin") -eq $true) {
+        if ($Uninstall -eq $true) {
+            Remove-Item -Recurse -Path "$SteamVrPath\drivers\$DriverFolder"
+            return 0
+        }
+        Copy-Item -Recurse -Force -Path $DriverPath -Destination "$SteamVrPath\drivers"
         return 0
     }
 }
