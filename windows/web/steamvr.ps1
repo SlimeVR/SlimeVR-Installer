cf3da684c5c2651224aa2cf969a2ee1c095c49be
Yury <rip95_95@mail.ru>
2021-09-23 22:31:51 +0300

    Prune externally registered SlimeVR drivers and possibly invalid drivers

--- windows/web/steamvr.ps1
+++ windows/web/steamvr.ps1
@@ -5,6 +5,28 @@ param (
     [parameter(Position=2)][switch]$Uninstall = $false
 )
 
+# Prune external SlimeVR driver(s) and possibly invalid entries
+$OpenVrConfig = Get-Content -Path "$env:LOCALAPPDATA\openvr\openvrpaths.vrpath" | ConvertFrom-Json
+$DriverPaths = @()
+if ($OpenVrConfig.external_drivers.Length) {
+    foreach ($DriverPath in $OpenVrConfig.external_drivers) {
+        if (-not (Test-Path -Path "$DriverPath\driver.vrdrivermanifest")) {
+            continue
+        }
+        $DriverManifest = Get-Content -Path "$DriverPath\driver.vrdrivermanifest" | ConvertFrom-Json
+        if ($DriverManifest.name -eq "SlimeVR") {
+            continue
+        }
+        $DriverPaths += $DriverPath
+    }
+}
+if ($DriverPaths.Length -eq 0) {
+    $OpenVrConfig.external_drivers = $null
+} else {
+    $OpenVrConfig.external_drivers = $DriverPaths
+}
+ConvertTo-Json -InputObject $OpenVrConfig | Out-File -FilePath "$env:LOCALAPPDATA\openvr\openvrpaths.vrpath"
+
 $DriverFolder = Split-Path -Path $DriverPath -Leaf
 $SteamVrPath = "$SteamPath\steamapps\common\SteamVR"
 if ((Test-Path -Path "$SteamVrPath\bin") -eq $true) {
